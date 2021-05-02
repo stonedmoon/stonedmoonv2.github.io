6e34962f16682a8efa609a2e2b1fbb214363126e
Stoned Moon <safeshitforks@gmail.com>
2021-05-02 01:17:15 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "StonedMoon",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
